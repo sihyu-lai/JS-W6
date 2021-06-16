400e2af52723110bba0cae146e7bfb513dd349ed
justrecommand <77605403+justrecommand@users.noreply.github.com>
2021-06-16 22:18:51 +0800

    JS第六週－支線任務
    目前只有介面
    無RWD

---
+++ JS-W6-minortask 未加js/js/all.js
@@ -0,0 +1,19 @@
+axios.get('https://raw.githubusercontent.com/hexschool/js-traninging-week6API/main/data.json')
+  .then(function (response) {
+    console.log(response.data);
+  
+  });
+
+  // 目的：先做出最佳組別排名
+
+/*
+codepenUrl: "https://codepen.io/manyu1225/pen/wvoQXvr?editors=1011"
+haveTen: "是"
+jsGroup: "1"
+practiceMinute: "15"
+practiceSecond: "8"
+slackName: "Mandy"
+timestamp: "2021/3/10 下午 10:11:17"
+youtubeUrl: ""
+
+*/ 
